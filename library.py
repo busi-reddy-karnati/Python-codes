2755665988e248b58ba970fc159d0c4da06a9593
busisatya@gmail.com <busisatya@gmail.com>
2021-12-08 20:36:41 -0500

    Adding Trie Class and DisjointSet

--- library.py
+++ library.py
@@ -1,8 +1,67 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = [0]*n
+        for i in range(n):
+            self.parent[i] = i
+
+    def are_connected(self, a, b):
+        if self.find(a) == self.find(b):
+            return True
+        return False
+
+    def union(self, a, b):
+        root1 = self.find(a)
+        root2 = self.find(b)
+        if root2 == root1:
+            return
+        self.parent[root1] = b
+
+    def find(self, node):
+        while self.parent[node] != node:
+            node = self.parent[node]
+        return node
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = []
+        self.is_end = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, string):
+        root = self.root
+        for char in string:
+            if char not in root.children:
+                root.children[char] = TrieNode()
+            root = root.children[char]
+        root.is_end = True
+
+    def is_there(self, string):
+        root = self.root
+        for char in string:
+            if char not in root.children:
+                return False
+            root = root.children[char]
+        return root.is_end
+
+    def is_prefix(self, string):
+        root = self.root
+        for char in string:
+            if char not in root.children:
+                return False
+            root = root.children[char]
+        return True
+
+
 def first_occurance(a, target):
     """
 
     :param a: array
-    :param target: target integet
+    :param target: target integer
     :return: index of first occurance of a number or -1
     """
     hi = len(a) - 1
@@ -63,3 +122,16 @@ def generate_permutations(nums, start):
         # Swap again
         nums[start], nums[i] = nums[i], nums[start]
     return ret
+
+
+def main():
+    d = DisjointSet(5)
+    print(d.are_connected(1, 2))
+    print("Adding 1,2")
+    d.union(1, 2)
+    print(d.are_connected(1, 2))
+
+
+if __name__ == '__main__':
+    main()
+
